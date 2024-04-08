1f45021b9b7b7c9f1d5996043884d0a2db6e7e7f
Go0107 <srfiveu.0107@gmail.com>
2024-04-08 12:40:23 +0900

    見た目だけの実装

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./components/layouts/header";
+import Footer from "./components/layouts/footer";
+import AuthenticatedHeader from "./components/layouts/authenticated_header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +14,27 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <head>
+        <meta charset="utf-8" />
+        <title>Conduit</title>
+        <link
+          href="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link
+          href="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic"
+          rel="stylesheet"
+          type="text/css"
+        />
+        <link rel="stylesheet" href="//demo.productionready.io/main.css" />
+      </head>
+      <body className={inter.className}>
+        {/* <AuthenticatedHeader /> */}
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
